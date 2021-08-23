5ebd204fa036b730e76409d39ab790ecfe39fa4f
Swaraj Rao <swaraj.rao@whoop.com>
2021-08-23 11:26:31 -0400

    Updating step

---
+++ send.py
@@ -0,0 +1,53 @@
+import os
+import requests
+import re
+
+jira_ticket_pattern = "[a-zA-Z]{1,}-\d{1,}"
+message = os.environ.get('message')
+branch = os.environ.get('branch')
+repository = os.environ.get('repository')
+build_num = os.environ.get('build_num')
+commit_sha = os.environ.get('commit_sha')
+status = os.environ.get('status')
+started_at = os.environ.get('started_at')
+completed_at = os.environ.get('completed_at')
+total_duration_milliseconds = os.environ.get('total_duration')
+build_url = os.environ.get('build_url')
+github_username = os.environ.get('github_username')
+EZDEPLOY_AUTH_TOKEN = os.environ.get('EZDEPLOY_AUTH_TOKEN')
+ticket = None
+branch_match = re.search(jira_ticket_pattern, branch)
+message_match = re.search(jira_ticket_pattern, str(message))
+if branch_match is not None:
+ticket=branch_match.group(0)
+elif message_match is not None:
+ticket=message_match.group(0)
+payload = {
+    "repository": repository,
+    "branch": branch,
+    "build_num": build_num,
+    "commit_sha": commit_sha,
+    "status": status,
+    "started_at": started_at,
+    "completed_at": completed_at,
+    "total_duration_milliseconds": total_duration_milliseconds,
+    "jira_ticket": ticket,
+    "build_url": build_url,
+    "github_username": github_username
+}
+request_headers = {'Authorization': EZDEPLOY_AUTH_TOKEN}
+if $environment is 'dev':
+url = https://api.dev.whoop.com/ez-deploy-service/v1/builds
+elif $environment is 'prod':
+url = https://api.prod.whoop.com/ez-deploy-service/v1/builds
+r = requests.post(url, json=payload, headers=request_headers)
+print("response:")
+print(r)
+print("content:")
+print(r.text)
+if r.status_code != 200:
+    print(r.content)
+    sys.exit(1)
+else:
+    print("Build successfully sent!")
+    sys.exit(0)
