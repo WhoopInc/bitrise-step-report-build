9050add6785d65f6c0c85f5d20535756ae7aeb9f
Swaraj Rao <swaraj.rao@whoop.com>
2021-08-23 17:29:35 -0400

    where am i

--- step.sh
+++ step.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-set -ex
+set -exv
 
 echo "Running python script"
+pwd
 ls
+echo $(pwd)
+echo $(ls)
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${CURR_DIR}"
+
+pwd
+ls
+echo $(pwd)
+echo $(ls)
+
 python ./send.py
 if [ $? != 0 ];
 then
