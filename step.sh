434be40ef8c58cfe0f65e7312c3d6477becb73fd
Swaraj Rao <swaraj.rao@whoop.com>
2021-08-23 17:40:22 -0400

    Using requirements.txt

--- step.sh
+++ step.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -exv
 
+echo 'Installing dependencies...'
+pip install -r requirements.txt
+
+echo 'cd-ing to step directory'
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${CURR_DIR}"
 
-
+echo 'running script'
 python ./send.py
+
 if [ $? != 0 ];
 then
     echo "exit 1"
