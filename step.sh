25e7df9f180776e657e7ba8561cc73b012c5968d
Swaraj Rao <swaraj.rao@whoop.com>
2021-09-02 11:50:19 -0400

    Using pip3, echoing env vars

--- step.sh
+++ step.sh
@@ -6,7 +6,13 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${CURR_DIR}"
 
 echo 'installing dependencies'
-pip install requests
+pip3 install requests
+
+echo 'listing time metrics'
+echo "${STARTED_AT}"
+echo "${COMPLETED_AT}"
+echo "${TOTAL_DURATION}"
+echo "${$BITRISE_GIT_MESSAGE}"
 
 echo 'executing script'
 python3 ./send.py
